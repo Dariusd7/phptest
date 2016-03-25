5e2cb49898f40c75b61dfe8ae40358edb26af47f
Scott Minter <scottminter@A003236.local>
2016-03-25 15:44:18 -0400

    removal all

--- test.php
+++ test.php
@@ -1,20 +1,3 @@
 <?php
 
-echo "Hello there. Can you modify me?\n";
-echo "this is another commit here.\n";
-
-echo "whats up Scott.\n";
-
-for($i = 0; $i < 10; $i++)
-{
-	echo "not much dude!\n";
-	echo "will you create conflict?!\n";
-	echo "you cant count dude!!!\n";
-}
-
-
-$x = 1;
-$y = 2;
-$z = $x + $y;
-echo $x . " + " . $y . " = " . $z . "\n";
 ?>
