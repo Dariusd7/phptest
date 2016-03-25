ac892cd8d10928b062702ec1865ce5732e9851fe
Scott Minter <scottminter@A003236.local>
2016-03-25 15:23:17 -0400

    I added a for() loop

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
 
-echo "Hello there. Can you modify me?\n"
+echo "Hello there. Can you modify me?\n";
 echo "this is another commit here.\n";
 
 echo "whats up Scott.\n";
+
+for($i = 0; $i < 10; $i++)
+{
+	echo "not much dude!\n";
+}
 
 ?>
